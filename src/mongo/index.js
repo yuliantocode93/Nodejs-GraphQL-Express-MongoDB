d32fac2a9bc6f907dd5140ac2d8e163a622bc310
yulianto <jooliantoo@gmail.com>
2024-03-28 11:08:37 +0700

    add mongodb connection from sample_mflix

---
+++ src/mongo/index.js
@@ -0,0 +1,25 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+const uri = "mongodb+srv://jooliantoo:W2kI9Ue66OzgFQqh@cluster0.ffnjbhs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    const movies = await client.db("sample_mflix").collection("movies").find().toArray();
+    console.log(movies);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
